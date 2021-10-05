8f1e374888933473a3985aa238217cef346330ae
thenablyn <ppicapietra@gmail.com>
2021-10-05 00:58:17 -0300

    1. Add a Provideo with getContactList and ContactList values 2. Create ContactDetail Component 3. Move stored data from local JSON to an API call end-point

--- src/views/Home/components/SearchResults/SearchResultItem.js
+++ src/views/Home/components/SearchResults/SearchResultItem.js
@@ -1,17 +1,17 @@
-export default function SearchResultItem ({ contact }) {
+export default function SearchResultItem({ contact }) {
 
     return (
         <div>
             <div className="list-group p-1">
-                            <div className="list-group-item list-group-item-action p-3">
-                                <h3>{contact.name} - ({contact.username})</h3>
-                                <p><b>Address:</b> {`${contact.address.street}, ${contact.address.suite}`} ({contact.address.city}) <span><a href={`http://www.google.com/maps/place/${contact.address.geo.lat},${contact.address.geo.lng}`}>show in map</a></span></p>
-                                <p><b>Phone:</b> <a href={`call: ${contact.phone}`}>{contact.phone}</a></p>
-                                <div className="d-flex justify-content-end">
-                                    <button className="btn btn-info me-0" onClick={() => {}}>See details</button>
-                                </div>
-                            </div>
-                        </div>
+                <div className="list-group-item list-group-item-action p-3">
+                    <h3>{contact.name} - ({contact.username})</h3>
+                    <p><b>Address:</b> {`${contact.address.street}, ${contact.address.suite}`} ({contact.address.city}) <span><a href={`http://www.google.com/maps/place/${contact.address.geo.lat},${contact.address.geo.lng}`}>show in map</a></span></p>
+                    <p><b>Phone:</b> <a href={`call: ${contact.phone}`}>{contact.phone}</a></p>
+                    <div className="d-flex justify-content-end">
+                        <button className="btn btn-info me-0" onClick={() => { }}>See details</button>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 }
