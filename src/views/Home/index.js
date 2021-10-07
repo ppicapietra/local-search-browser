5f16c9c4cd020111957740720f911a9310380805
thenablyn <ppicapietra@gmail.com>
2021-10-07 10:43:09 -0300

    minor changes

--- src/views/Home/index.js
+++ src/views/Home/index.js
@@ -1,19 +1,22 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useContext } from "react";
+import ContactsContext from "../../context/contacts";
 import SearchBox from "./components/SearchBox";
 import "./style.css";
 import SearchResults from "./components/SearchResults";
 
 export default function Search() {
-    const [isAtTop, setIsAtTop] = useState(false);
-    const [results, setResults] = useState([]);
-    const [ usersData, setUsersData ] = useState([]);
+    const [ isAtTop , setIsAtTop ] = useState(false);
+    const [ results , setResults ] = useState([]);
+
+    const { getContactsList, contactsList } = useContext(ContactsContext);
 
     const handleSearchClick = (searchText) => {
         if (searchText) {
             setIsAtTop(true);
-            if (usersData?.length) {
+            if (contactsList?.length) {
+                console.log(contactsList);
                 const regE = new RegExp(searchText, "i");
-                const filteredData = usersData.filter((user) => {
+                const filteredData = contactsList.filter((user) => {
                     return (regE.test(Object.values(user).join('')));
                 });
                 setResults(filteredData);
@@ -22,14 +25,8 @@ export default function Search() {
     }
 
     useEffect(() => {
-        const getUsersData = async () => {
-            fetch("https://jsonplaceholder.typicode.com/users")
-            .then(response => response.json())
-            .then(data =>
-                setUsersData(data));
-        };
-        getUsersData().catch(null);
-    }, [])
+        getContactsList().catch(null);
+    }, []);
 
     return (
         <div className={`p-4 container search ${isAtTop ? "search--top" : "search--middle"}`}>
