47d01917c8b34d31e90cae50688a88d977ff322b
thenablyn <ppicapietra@gmail.com>
2021-10-09 04:51:38 -0300

    1. Replace useHistory for Link in SearchResultItem

--- src/views/Home/components/SearchResults/SearchResultItem.js
+++ src/views/Home/components/SearchResults/SearchResultItem.js
@@ -1,9 +1,6 @@
-import { useHistory } from "react-router";
+import { Link } from "react-router-dom";
 
 export default function SearchResultItem({ contact }) {
-
-    const history = useHistory();
-
     return (
         <div>
             <div className="list-group p-1">
@@ -12,7 +9,11 @@ export default function SearchResultItem({ contact }) {
                     <p><b>Address:</b> {`${contact.address.street}, ${contact.address.suite}`} ({contact.address.city}) <span><a href={`http://www.google.com/maps/place/${contact.address.geo.lat},${contact.address.geo.lng}`}>show in map</a></span></p>
                     <p><b>Phone:</b> <a href={`call: ${contact.phone}`}>{contact.phone}</a></p>
                     <div className="d-flex justify-content-end">
-                        <button className="btn btn-info me-0" onClick={() => { history.push(`/Detail/${contact.id}`)}}>See details</button>
+                        <button className="btn btn-info me-0">
+                            <Link to={`/Detail/${contact.id}`} className="text-decoration-none text-dark">
+                                See details
+                            </Link>
+                        </button>
                     </div>
                 </div>
             </div>
